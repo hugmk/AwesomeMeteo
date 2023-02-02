7a85e6343734c7720ade4e9a269c3d6256c547a9
hugmk <103774810+hugmk@users.noreply.github.com>
2023-02-02 19:41:09 +0100

    Fix city attributes type

--- classes/City.tsx
+++ classes/City.tsx
@@ -3,11 +3,11 @@ import Meteo from "./Meteo";
 export default class City {
 
     name: string;
-    latitude: string;
-    longitude: string;
+    latitude: number;
+    longitude: number;
     meteo: Meteo | undefined;
 
-    constructor(name: string, latitude: string, longitude: string, meteo?: Meteo | undefined) {
+    constructor(name: string, latitude: number, longitude: number, meteo?: Meteo | undefined) {
       this.name = name;
       this.latitude = latitude;
       this.longitude = longitude;
